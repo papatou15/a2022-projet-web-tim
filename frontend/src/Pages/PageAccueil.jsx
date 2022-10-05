d7c78c878aebeb2c035f0a7fc81d9b9bff2e6d49
Antoine Ridard <papatou15@hotmail.com>
2022-10-05 14:33:57 -0400

    branching pour Antoine

--- frontend/src/Pages/PageAccueil.jsx
+++ frontend/src/Pages/PageAccueil.jsx
@@ -3,6 +3,9 @@ import './PageAccueil.scss';
 export default function PageAccueil(props){
     return (
         <main className="PageAccueil">
+            <section>
+                
+            </section>
         </main>
     );
 }
