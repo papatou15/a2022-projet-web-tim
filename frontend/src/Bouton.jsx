d4e3e648cff81e5b78ea85adcda436bdad0e0016
Antoine Ridard <papatou15@hotmail.com>
2022-10-05 17:37:05 -0400

    Début du layout accueil

--- frontend/src/Bouton.jsx
+++ frontend/src/Bouton.jsx
@@ -2,7 +2,7 @@ import './Bouton.scss';
 
 export default function Bouton(props){
     return (
-        <a className="Bouton">
+        <a href="#" className="Bouton">
             Button
         </a>
     );
--- frontend/src/Pages/PageAccueil.jsx
+++ frontend/src/Pages/PageAccueil.jsx
@@ -1,10 +1,97 @@
 import './PageAccueil.scss';
+import Bouton from '../Bouton';
 
 export default function PageAccueil(props){
     return (
         <main className="PageAccueil">
-            <section>
-                
+            <section className="block1">
+                <div className="mainTitle">
+                    <h1>TIM Maisonneuve</h1>
+                    <h3>Accroche</h3>
+                </div>
+                <div className="mainButtons">
+                    <Bouton/>
+                    <Bouton/>
+                </div>
+            </section>
+            <section className="block2">
+                <div className="mainTitleCours">
+                    <h2>Tes <b>COURS</b></h2>
+                </div>
+                <div className="carouselSessions">
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                    <div className="carteSessions">
+                        <h4>Session 1</h4>
+                        <div className="listeCours">
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                            <a href="#" className="cours"><p>Cours</p></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="block3">
+
+            </section>
+            <section className="block4">
+
+            </section>
+            <section className="block5">
+
+            </section>
+            <section className="block6">
+
             </section>
         </main>
     );
