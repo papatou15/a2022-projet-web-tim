5fa957fd070c2d9ef38352a8d321668e7d003673
Antoine Ridard <papatou15@hotmail.com>
2022-10-09 19:17:45 -0400

    + de section dans la page d'acceuil

--- frontend/src/Navigation/Burger.jsx
+++ frontend/src/Navigation/Burger.jsx
@@ -3,7 +3,7 @@ import useFetch from '../Hooks/useFetch';
 
 export default function Burger(props){
 
-    const logoSrc = useFetch('http://localhost:8080/projet-web-5w5/index.php/wp-json/tim/v1/settings/logo');
+    const logoSrc = useFetch('http://timm184.sg-host.com/wp-json/tim/v1/settings/logo');
 
     return (
         <div className="Burger" onClick={props.onClick}>
--- frontend/src/Pages/PageAccueil.jsx
+++ frontend/src/Pages/PageAccueil.jsx
@@ -15,7 +15,7 @@ export default function PageAccueil(props){
                 </div>
             </section>
             <section className="block2">
-                <div className="mainTitleCours">
+                <div className="titleSections">
                     <h2>Tes <b>COURS</b></h2>
                 </div>
                 <div className="carouselSessions">
@@ -90,18 +90,107 @@ export default function PageAccueil(props){
                 </div>
             </section>
             <section className="block3">
-                <div className="mainTitleAvenir">
+                <div className="titleSections">
                     <h2>Ton <b>AVENIR</b></h2>
                 </div>
-                <div>
-                    
+                <div className="contenuAvenir">
+                    <div className="blockAvenir">
+                        <div className="avenirSlides Entreprise">
+                            <div className="avenirSlidesContent">
+                                <h3>En <b>Entreprise</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                        <div className="avenirSlides Universite">
+                            <div className="avenirSlidesContent">
+                                <h3>À l'<b>Université</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                        <div className="avenirSlides Stage">
+                            <div className="avenirSlidesContent">
+                                <h3>En <b>Stage</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                    </div>
                 </div>
+                
             </section>
             <section className="block4">
-
+                <div className="titleSections">
+                    <h2>Un <b>aperçu</b> des <b>PROJETS</b></h2>
+                </div>
+                <div className="carouselCartesProjets">
+                    <div className="carteProjet">
+                        <h4 className="titreProjet">Titre Projet</h4>
+                        <img src="" alt="" className="imgProjet" />
+                        <p>Type</p>
+                        <p>Cours</p>
+                        <p>Auteurs</p>
+                    </div>
+                    <div className="carteProjet">
+                        <h4 className="titreProjet">Titre Projet</h4>
+                        <img src="" alt="" className="imgProjet" />
+                        <p>Type</p>
+                        <p>Cours</p>
+                        <p>Auteurs</p>
+                    </div>
+                    <div className="carteProjet">
+                        <h4 className="titreProjet">Titre Projet</h4>
+                        <img src="" alt="" className="imgProjet" />
+                        <p>Type</p>
+                        <p>Cours</p>
+                        <p>Auteurs</p>
+                    </div>
+                    <div className="carteProjet">
+                        <h4 className="titreProjet">Titre Projet</h4>
+                        <img src="" alt="" className="imgProjet" />
+                        <p>Type</p>
+                        <p>Cours</p>
+                        <p>Auteurs</p>
+                    </div>
+                    <div className="carteProjet">
+                        <h4 className="titreProjet">Titre Projet</h4>
+                        <img src="" alt="" className="imgProjet" />
+                        <p>Type</p>
+                        <p>Cours</p>
+                        <p>Auteurs</p>
+                    </div>
+                </div>
             </section>
             <section className="block5">
-
+                <div className="titleSections">
+                    <h2>Le <b>SOCIAL</b></h2>
+                </div>
+                <div className="contenuSocial">
+                    <div className="blockSocial">
+                        <div className="socialSlides Event">
+                            <div className="socialSlidesContent">
+                                <h3>Les <b>Événements</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                        <div className="socialSlides Communaute">
+                            <div className="socialSlidesContent">
+                                <h3><b>Communauté</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                        <div className="socialSlides Maisonneuve">
+                            <div className="socialSlidesContent">
+                                <h3><b>Maisonneuve</b></h3>
+                                <Bouton></Bouton>
+                            </div>
+                            <div className="clipPath"></div>
+                        </div>
+                    </div>
+                </div>
             </section>
             <section className="block6">
 
