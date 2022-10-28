4eb08dab2aac4c66dd8c57c0e9eb12d49a8ea54f
Antoine Ridard <papatou15@hotmail.com>
2022-10-28 16:31:24 -0400

    Expand carte de projet

--- frontend/src/Composants/CarteProjet.jsx
+++ frontend/src/Composants/CarteProjet.jsx
@@ -1,20 +1,31 @@
 import './CarteProjet.scss';
+import { useContext, useState } from 'react';
+import ExpanderButton from '../Navigation/ExpanderButton';
+import ExpanderSection from '../Navigation/ExpanderSection';
 
 export default function CarteProjet({projet, titre, type, cours, auteurs, image}){
-
+    const [expanderState, setExpanderState] = useState(false);
 
     return(
-        <div className="carteProjet">
-            <div className="titreProjet">
-                <h4>{titre}</h4>
+        <div>
+            <ExpanderButton expanderState={expanderState} setExpanderState={setExpanderState}>
+            <div className="carteProjet">
+                <div className="titreProjet">
+                    <h4>{titre}</h4>
+                </div>
+                <img src={image} alt="" className="imgProjet"/>
+                <div className="descProjet">
+                    <p>Catégorie: {type}</p>
+                    <p>Cours: {cours}</p>
+                    <p>Fait par: {auteurs}</p>
+                </div>
             </div>
-            <img src={image} alt="" className="imgProjet"/>
-            <div className="descProjet">
-                <p>Catégorie: {type}</p>
-                <p>Cours: {cours}</p>
-                <p>Fait par: {auteurs}</p>
-            </div>
-            
+            </ExpanderButton>
+            <ExpanderSection expanderState={expanderState}>
+                <div>
+                    Allo
+                </div>
+            </ExpanderSection>
         </div>
     );
 }
