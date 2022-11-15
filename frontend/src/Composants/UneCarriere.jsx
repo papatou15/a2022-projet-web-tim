324bfbef0f9cd3a4f57caa73334e031b9486df24
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-11-15 11:55:11 -0500

    Page enseignant

--- frontend/src/Composants/UneCarriere.jsx
+++ frontend/src/Composants/UneCarriere.jsx
@@ -1,10 +1,10 @@
 import './UneCarriere.scss';
 
-export default function UneCarriere({nom, sorte}){
+export default function UneCarriere({nom, sorte, lien}){
     return (
         <div className="UneCarriere">
             <p className='nom-emploi'>{nom}</p>
-            <p>Voir plus</p>
+            <a href={lien} target="_blank" rel="noopener noreferrer">Voir plus</a>
         </div>
     );
 }
