10ccc7a8ab33bb3bd68ee049dcb32a6887dcd15a
Gui-Vez <vezina.guillaume@outlook.com>
2022-12-08 21:54:26 -0500

    Ajout images temporaires Page Social

--- frontend/src/Composants/Sections/Social/MaisonneuveSection.jsx
+++ frontend/src/Composants/Sections/Social/MaisonneuveSection.jsx
@@ -13,10 +13,10 @@ export default function MaisonneuveSection({backgroundColor, texte1, texte2, lie
                     <img src="https://cdn.discordapp.com/attachments/1012793479648251914/1044344188952641557/persofinal-balloon.png" alt="alternatetext"></img>
                     <div className="sideContent">
                         <div className="content1">
-                            <p>{texte1}<b><a href={lien1} target="_blank" rel="noopener noreferrer">{lienPlaceholder1}</a></b></p>
+                            <p>{texte1} <b><a href={lien1} target="_blank" rel="noopener noreferrer">{lienPlaceholder1}</a></b></p>
                         </div>
                         <div className="content2">
-                            <p>{texte2}<b><a href={lien2} target="_blank" rel="noopener noreferrer">{lienPlaceholder2}</a></b></p>
+                            <p>{texte2} <b><a href={lien2} target="_blank" rel="noopener noreferrer">{lienPlaceholder2}</a></b></p>
                         </div>
                     </div>
                 </div>
